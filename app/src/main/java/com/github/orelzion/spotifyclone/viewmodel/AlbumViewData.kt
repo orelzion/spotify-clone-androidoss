a56cda25103e18e2d38be7b5330def4aefdc3a9c
orelzion <orelzion@gmail.com>
2021-01-26 21:36:36 +0200

    view model

---
+++ app/src/main/java/com/github/orelzion/spotifyclone/viewmodel/AlbumViewData.kt
@@ -0,0 +1,9 @@
+package com.github.orelzion.spotifyclone.viewmodel
+
+data class AlbumViewData(
+    val id: String,
+    val imageUrl: String,
+    val title: String,
+    val artistName: String,
+    val totalTracks: Int
+)
